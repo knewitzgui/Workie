dda6b979ecc212cd85c5bca9f48fe6ee65b26061
Guilherme Maier Knewitz <knewitzgui@gmail.com>
2020-05-25 22:21:51 -0300

    CI

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var formatPhone = require('./src/maskPhone');
 var formatCep = require('./src/maskCep');
 var consultaCep = require('./src/consultaCep');
 
-// consultaCep('dev', '93315400')
-//   .then(address => console.log(address))
-//   .catch(err => console.log(err))
+consultaCep('dev', '93315400')
+  .then(address => console.log(address))
+  .catch(err => console.log(err))
 // console.log(formatPhone(51993150226))
 // console.log(formatCpfCnpj(22185237012));
 // console.log(formatCep(93315400));
